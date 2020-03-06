e7084fe26995f8f054c155a3944111995d625a0c
Caleb Lagge <caleb.i.lagge-1@ou.edu>
2020-03-05 22:15:43 -0600

    Adding fire data set

---
+++ R/fire.R
@@ -0,0 +1,13 @@
+#' FFire data set
+#'
+#' A dataset with 2 quantative variables
+#' The interest is the damage to homes as the distance from the nearest fire station varies
+#'
+#' @format A data frame with 15 rows 2 variables:
+#' \describe{
+#' \item{DISTANCE}{The distance of the fire from the fire station}
+#' \item{Damage}{Damage in dollars X1000}
+#' ...
+#' }
+#' @source \url{{https://www.crcpress.com/Statistics-for-Engineering-and-theSciences/Mendenhall-Sincich/p/book/9781498728850}
+#' "fire"
